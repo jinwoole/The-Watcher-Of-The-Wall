51baa50e39aa8fe88befc7424ecc63c089e6b78f
lee <lee@localhost.localdomain>
2024-04-28 22:06:11 +0900

    Init something and else

--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -1,10 +1,23 @@
-<script>
-    import { Alert } from 'flowbite-svelte';
-  </script>
-  
-  <div class="p-8">
-    <Alert>
-      <span class="font-medium">Info alert!</span>
-      Change a few things up and try submitting again.
-    </Alert>
-  </div>
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import { axiosStore } from '../stores/axiosStore';
+  import type { AxiosInstance } from 'axios';
+
+  let axiosInstance: AxiosInstance;
+
+  // 스토어 구독
+  axiosStore.subscribe((instance) => {
+    axiosInstance = instance;
+  });
+
+  onMount(() => {
+    // GET 요청
+    axiosInstance.get('/vms') // API 엔드포인트
+      .then((response) => {
+        console.log('응답:', response.data); // 응답 데이터 처리
+      })
+      .catch((error) => {
+        console.error('오류:', error); // 오류
+      });
+  });
+</script>
